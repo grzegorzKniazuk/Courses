a8d11e2026acaa3bcdf17a7c09f0178fbfdc7db2
Grzegorz Kniazuk <grzegorz.kniazuk@gmail.com>
2018-08-22 21:24:21 +0200

    commit 22.08

--- JavaScript/Kurs_EcmaScript_6_Nowy_Standard_JavaScript.js
+++ JavaScript/Kurs_EcmaScript_6_Nowy_Standard_JavaScript.js
@@ -96,7 +96,7 @@ let numbers = [12, 3, 9, 22, 11, 6];
 let numbers2 = [2, 33, 10, ...numbers, 1, 75];
 Math.max(...numbers);
 let letters = [..."Grzegorz"]; // ['G', 'r', 'z', 'e', 'g', 'o', 'r', 'z'];
-*/
+
 // destructuring
 // dekompozycja obiektow
 let person = {
@@ -111,3 +111,98 @@ let { firstName: fName, lastName: lName, age: a } = person; // dekompozycja do i
 console.log(firstName, lastName, age); // Jan Kowalski 49
 
 // dekompozycja tablic
+let numbers = [10, 20, 30, 40, 50];
+let [one, two, three, , five] = numbers; // puste przecinki, pomijanie wartosci
+
+// value swapping - zamiana wartosci
+let a = 1, b = 2;
+[a, b] = [b, a];
+
+// dekompozycja zagniezdzonych struktur
+let person = {
+    firstName: 'Jan',
+    lastName: 'Kowalski',
+    age: 49,
+    job: {
+        name: 'Programista',
+        experience: 20,
+    },
+    favNumbers: {
+        list: [2, 7, 3],
+    }
+};
+let { firstName, age } = person || {};
+let { job: { name }} = person || {}; // Programista
+let { favNumbers: { list: [ , second ]}} = person || {}; // 7
+
+// Domyślne wartości i operator rest
+let numbers = [10, 20, 30, 40, 50];
+let [first, second, ...rest] = numbers || []; // rest => [30, 40, 50]
+
+let number = [10];
+let [one, two = 2] = number || []; // wartosc domyslna
+
+let person = {
+    firstName: 'Jan',
+    lastName: 'Kowalski',
+    age: 49,
+    job: "Księgowy",
+};
+let { firstname, lastname, job = 'programista'} = person || {}; // domyslna wartosc dla job
+
+// Dekompozycja parametrów funkcji
+function setSliderSpeed({ speed, easing } = {}) { // z domyslnym pustym obiektem
+    let slider = {};
+    slider.speed = speed;
+    slider.easing = easing;
+}
+const config = {
+    autoPlay: true,
+    speed: 500,
+    pause: 2000,
+    easing: 'linear',
+    infinite: true,
+};
+setSliderSpeed(config);
+
+// Template strings
+// tag functions - Funkcje tagujące
+function formatPrice(strings, ...values) {
+    console.log(strings); // [ dekompozycja stringa tak jak przy pomocy split ]
+    console.log(values); // wartosci interpolowane
+    let output = '';
+    strings.forEach((string, index) => {
+        output += string + values[index];
+    })
+}
+
+let product = {
+    name: "Płyta DVD",
+    price: 1,
+};
+let { name, price } = product || {};
+let info = formatPrice`Dodałeś do koszyka produkt ${name} w cenie ${price}`; // tagowanie za pomoca funkcji
+
+// nowe metody dla string
+const url = 'http//mojastrona.pl';
+url.startsWith('h'); // true
+url.endsWith('s'); // false
+url.includes('moja'); // true
+url.repeat(2); // powtorzenie 2x stringu
+
+// klasy
+// klasy nie sa hoistowane
+// klasy nie sa wlasciwosciami obiektu Window
+// tworzenie klas
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    sayHello() {
+        return `Witaj, ${this.firstname} ${this.lastname}`;
+    }
+}
+*/
+
+
