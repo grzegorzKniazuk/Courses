693d2f7b1c04bdd6d0dab85315d6c9e129efe7a7
= <grzegorz.kniazuk@gmail.com>
2018-12-14 21:00:24 +0100

    update 14.12

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -12,35 +12,54 @@ module.exports = { // config
     detool: 'source-map', // wskazywanie na zrodlowa linie bledu w konsoli developerskiej przegladarki
     module: { // loaders settings
         rules: [{
-            test: /\.csv$/, // rozszerzenie ktore dotyczy tej reguly
-            use: [
-                {
-                    loader: 'to-string-loader',
-                },
-                { // paczki do uzycia
-                    loader: 'csv-loader',
-                    options: {
-                        header: true, // pierwsza linia pliku to nazwy kolumn
+                test: /\.csv$/, // rozszerzenie ktore dotyczy tej reguly
+                use: [
+                    {
+                        loader: 'to-string-loader',
+                    },
+                    { // paczki do uzycia
+                        loader: 'csv-loader',
+                        options: {
+                            header: true, // pierwsza linia pliku to nazwy kolumn
+                        }
                     }
-                }
-            ],
+                ],
             exclude: /node_modules/,
-        }, {
-            test: /\.js$/, // pliki js
-            use: {
-                loader: 'babel-loader', // uzycie babela
-                options: {
-                    presets: [ 'es2015' ],
-                    plugins: [
-                        'babel-plugin-transform-class-properties',
-                    ],
-                }
-            },
+            }, {
+                test: /\.js$/, // pliki js
+                use: {
+                    loader: 'babel-loader', // uzycie babela
+                    options: {
+                        presets: [ 'es2015' ],
+                        plugins: [
+                            'babel-plugin-transform-class-properties',
+                        ],
+                    }
+                },
+            }, { // dolaczanie css
+                test: /\.css$/,
+                use: [ 'style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true, // ułatwia debugowanie w devtools
+                        minimize: true, // zmniejsza obietosc pliku css
+                    },
+                }, {
+                    loader: 'postcss-loader', // instalacja postcss w celu uzycia autoprefixera
+                    options: {
+                        plugins: (loader) => {
+                            new require('autoprefixer')()
+                        }
+                    }
+                }],
+            }, { // obsluga plikow scss
+                test: /\.scss/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ],
         }
         ],
     },
     plugins: [
         new UglifiJSPPlugin(),
         new ExtractTextPlugin(),
-    ]
+    ],
 };
