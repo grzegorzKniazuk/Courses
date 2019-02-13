08a9ab784ac6e4faaadfbe0f5bbedea7ae25ba30
= <grzegorz.kniazuk@gmail.com>
2019-02-13 21:13:28 +0100

    update 13.02 - rxjs

--- RxJs/RxJs 6 in practice/src/notes.ts
+++ RxJs/RxJs 6 in practice/src/notes.ts
@@ -1,9 +1,23 @@
 import {concat, fromEvent, interval, merge, noop, Observable, of, timer} from "rxjs";
 import {Course} from "./app/model/course";
-import {concatMap, exhaustMap, filter, map, mergeMap, shareReplay, tap} from "rxjs/operators";
+import {
+    catchError,
+    concatMap,
+    debounceTime, delayWhen,
+    distinctUntilChanged,
+    exhaustMap,
+    filter, finalize,
+    map,
+    mergeMap, retryWhen,
+    shareReplay, startWith, switchMap,
+    tap, throttle, throttleTime
+} from "rxjs/operators";
 import {FormGroup} from "@angular/forms";
 import {fromPromise} from "rxjs/internal-compatibility";
 import {ElementRef, ViewChild} from "@angular/core";
+import {Lesson} from "./app/model/lesson";
+import {createHttpObservable} from "./app/common/util";
+import {ActivatedRoute, Router} from "@angular/router";
 
 class Notes {
 
@@ -11,14 +25,19 @@ class Notes {
     private readonly interval$ = interval(1000); // Observable<number> // definicja strumienia
     private readonly timer$ = timer(3000, 1000); // delay, interval
     private readonly click$ = fromEvent(document, 'click'); // Observable from event
-    private readonly courses$: Observable<Course[]> = this.mapOperator();
+    private courses$: Observable<Course[]>;
+    private lessons$: Observable<Lesson[]>;
     private beginnersCourses: Course[];
     private beginnersCourses$: Observable<Course[]>;
     private advancedCourses: Course[];
     private advancedCourses$: Observable<Course[]>;
     private form: FormGroup;
+    private courseId: string;
 
     @ViewChild('saveButton') public saveButton: ElementRef;
+    @ViewChild('searchInput') public searchInput: ElementRef;
+
+    constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
     public basics(): void {
         this.click$.subscribe(
@@ -38,28 +57,65 @@ class Notes {
             () => { console.log('complete') });
     }
 
+    // cancelable http observable
     public httpObservable(url: string): Observable<Course> {
         return Observable.create((observer) => {
-            fetch(url).then((response) => {
-                return response.json();
+
+            const controller = new AbortController(); // from fetchAPI
+            const signal = controller.signal;
+
+            fetch(url, { signal }).then((response) => {
+
+                if (response.ok) { // error handler
+                    return response.json();
+                } else {
+                    observer.error(`Request failed with status code: ${response.status}`);
+                }
+
+
             }).then((body) => {
                 observer.next(body);
                 observer.complete();
-            }).catch((error) => {
+            }).catch((error) => { // only fatal errors (browser, network)
                 observer.error(error);
             });
+
+            return () => { // observable cancel function
+                return controller.abort();
+            }
         });
     }
 
     // Map transform the items emitted by an Observable by applying a function to each item
 
     // shareReplay - zapobiega tworzeniu nowych strumieni dla kazdej z nowych subskrypcji (np dla http - kilka takich samych strzalow)
-    public mapOperator(): Observable<Course[]> {
+    public mapOperator() {
+        return this.http$.pipe(
+            catchError((error) => { // error handling
+                console.log(error);
+                return of([]) // try to recover data (here empty array)
+            }),
+            finalize(() => { // completes or error out
+                console.log('finalize executed');
+            }),
+            tap(() => console.log('HTTP request executed')),
+            map(response => Object.values(response['payload'])),
+            shareReplay(), // shareReplay
+        );
+    }
+
+    // retry error strategy
+    public retryErrorStrategy() {
         return this.http$.pipe(
             tap(() => console.log('HTTP request executed')),
-            map((response) => Object.values(response['payload']),
-            shareReplay() // shareReplay
-        ));
+            map(response => Object.values(response['payload'])),
+            shareReplay(), // shareReplay
+            retryWhen((errors) => errors.pipe( // retry error strategy
+                delayWhen(() => { // wyslij zapytanie ponownie po 2s
+                    return timer(2000)
+                })
+            ))
+        );
     }
 
     // imperative design
@@ -141,5 +197,45 @@ class Notes {
             .pipe(
                 exhaustMap(() => this.saveCourse(this.form.value)) // ignore repeated clicks
             )
+    }
+
+
+    public cancelableObservable(): void {
+        const interval1$ = interval(1000);
+
+        const sub = interval1$.subscribe(console.log);
+
+        setTimeout(() => sub.unsubscribe(), 5000);
+    }
+
+    // switchMap, commonly used for implementing search
+    // Maps each value to an Observable, then flattens all of these inner Observables using switch
+    public switchMap(): void {
+        this.courseId = this.activatedRoute.snapshot.params['id'];
+
+        this.courses$ = createHttpObservable(`/api/courses/${this.courseId}`);
+
+
+        const searchLessons$ = fromEvent(this.searchInput.nativeElement, 'keyup')
+            .pipe(
+                map((event: any) => event.target.value),
+                startWith(''),
+                debounceTime(500), // waiting for a value to become stable
+                throttleTime(1000),
+                throttle(() => interval(1000)), // like throttleTime but silence duration is deermined by a second Observable
+                distinctUntilChanged(), // no longer duplicate values in our output
+                switchMap((search) => this.loadLessons(search))
+            );
+
+        const initialLessons$ = this.loadLessons();
+
+        this.lessons$ = concat(initialLessons$, searchLessons$);
+    }
+
+    public loadLessons(search?): Observable<Lesson[]> {
+        return createHttpObservable(`/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`)
+            .pipe(map((response) => {
+                return response['payload'];
+            }));
     }
 }
